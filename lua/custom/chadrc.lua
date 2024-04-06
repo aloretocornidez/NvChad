e50c4ceab5a524a5c3d761efdc5c04c08c7a5f9b
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2024-04-06 14:21:55 -0700

    fix merges

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "nightfox",
-  theme_toggle = { "nightfox", "one_light" },
+  theme = "blossom_light",
+  theme_toggle = { "blossom_light", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
